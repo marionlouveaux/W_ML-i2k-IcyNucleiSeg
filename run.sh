0e52f72965eaa77821e6f3cea3f6c60be9527617
rmaree <raphael.maree@ulg.ac.be>
2018-01-29 18:24:27 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #fullpaths for lib, jar, protocol
 #launch it from icy directory
 cd /icy/
-java -cp /icy/lib/ -jar /icy/icy.jar -hl -x plugins.adufour.protocols.Protocols protocol="/icy/protocols/icy_brigth_spot_detections_batch.protocol" inputFolder="/icy/data/in" extension=tif csvFileSuffix=_results scale3enable=true scale3sensitivity=40
+#java -cp /icy/lib/ -jar /icy/icy.jar -hl -x plugins.adufour.protocols.Protocols protocol="/icy/protocols/icy_brigth_spot_detections_batch.protocol" inputFolder="/icy/data/in" extension=tif csvFileSuffix=_results scale3enable=true scale3sensitivity=40
+
+java -cp /icy/lib/ -jar /icy/icy.jar -hl -x plugins.adufour.protocols.Protocols protocol="/icy/protocols/protocol.protocol" inputFolder="$1" extension=tif csvFileSuffix=_results scale3enable=true scale3sensitivity=$2
 
 #parameters can be exposed through read boxes in the icy protocols and identified with an id, here e.g. scale3enable
