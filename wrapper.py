5d82957c8578613dc83d01cd73fec4b0867a0c0c
GiMichiels <36195940+GiMichiels@users.noreply.github.com>
2018-04-20 13:06:26 +0200

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -1,32 +1,38 @@
-import sys
+import logging
 import os
-from cytomine import Cytomine
-from cytomine.models import *
-from subprocess import call
-from skimage import io
-import numpy as np
-from sldc import locator
-from shapely.geometry import Point
-from array import *
+import sys
 from argparse import ArgumentParser
+from subprocess import call
 
-def readcoords(fname):
-	X = []
-	Y = []
-	F = open(fname,'r')
-	i = 1
-	for index,l in enumerate(F.readlines()):
-		if index<2: continue
-		t = l.split('\t')
-		print(t)
-		if len(t)>1:
-			X.append(float(t[5]))
-			Y.append(float(t[6]))
-		i=i+1
-	F.close()
-	return X,Y
+import numpy as np
+from cytomine import CytomineJob
+from cytomine.models import Annotation, AnnotationTerm, Job, ImageInstanceCollection, Property
+from shapely.ops import cascaded_union
+from shapely.affinity import affine_transform
+from skimage import io
+from sldc import locator
 
-baseOutputFolder = "/dockershare/";
+def add_annotation(img_inst, polygon, label=None, proba=1.0):
+    image_id = img_inst.id
+    # Transform in cartesian coordinates
+    polygon = affine_transform(polygon, [1, 0, 0, -1, 0, img_inst.height])
+
+    annotation = Annotation(polygon.wkt, image_id).save()
+    if label is not None and annotation is not None:
+        annotation_term = AnnotationTerm()
+        annotation_term.annotation = annotation.id
+        annotation_term.annotationIdent = annotation.id
+        annotation_term.userannotation = annotation.id
+        annotation_term.term = label
+        annotation_term.expectedTerm = label
+        annotation_term.rate = proba
+        annotation_term.save()
+    return annotation
+
+def makedirs(path):
+    if not os.path.exists(path):
+os.makedirs(path)
+
 
 parser = ArgumentParser(prog="Icy-SpotDetection.py", description="Icy workflow to detect spots in 2D images")
 parser.add_argument('--cytomine_host', dest="cytomine_host", default='http://localhost-core')
@@ -34,93 +40,72 @@ parser.add_argument('--cytomine_public_key', dest="cytomine_public_key", default
 parser.add_argument('--cytomine_private_key', dest="cytomine_private_key", default="")
 parser.add_argument("--cytomine_id_project", dest="cytomine_id_project", default="5378")
 parser.add_argument("--icy_scale3sensitivity", dest="scale3sens", default="40")
+params, others = parser.parse_known_args(sys.argv)
 
-arguments, others = parser.parse_known_args(sys.argv)
-scale3sens = arguments.scale3sens
+base_path = "/icy/data"
 
-#Cytomine connection parameters
-cytomine_host=arguments.cytomine_host
+with CytomineJob(params.cytomine_host, params.cytomine_public_key, params.cytomine_private_key, params.cytomine_id_software, params.cytomine_id_project, verbose=logging.INFO) as cj:
+    cj.job.update(status=Job.RUNNING, progress=0, statusComment="Initialisation...")
 
-id_project=arguments.cytomine_id_project
+    working_path = os.path.join(base_path, str(cj.job.id))
+    in_path = os.path.join(working_path, "in")
+    makedirs(in_path)
+    out_path = os.path.join(working_path, "out")
+    makedirs(out_path)
+    gt_path = os.path.join(working_path, "ground_truth")
+    makedirs(gt_path)
 
-conn = Cytomine(arguments.cytomine_host, arguments.cytomine_public_key, arguments.cytomine_private_key, base_path = '/api/', working_path = '/tmp/', verbose= True)
+    cj.job.update(progress=1, statusComment="Downloading images...")
+    image_instances = ImageInstanceCollection().fetch_with_filter("project", params.cytomine_id_project)
+    input_images = [i for i in image_instances if gt_suffix not in i.originalFilename]
+    gt_images = [i for i in image_instances if gt_suffix in i.originalFilename]
 
-current_user = conn.get_current_user()
-user_job = current_user
+    for input_image in input_images:
+        input_image.download(os.path.join(in_path, "{id}.tif"))
 
-job = conn.get_job(user_job.job)
+    for gt_image in gt_images:
+        related_name = gt_image.originalFilename.replace(gt_suffix, '')
+        related_image = [i for i in input_images if related_name == i.originalFilename]
+        if len(related_image) == 1:
+            gt_image.download(os.path.join(gt_path, "{}.tif".format(related_image[0].id)))
 
-job = conn.update_job_status(job, status = job.RUNNING, progress = 0, status_comment = "Loading images from Cytomine...")
+    cj.job.update(progress=25, statusComment="Launching workflow...")
 
-# Get the list of images in the project
-image_instances = ImageInstanceCollection()
-image_instances.project  =  id_project
-image_instances  =  conn.fetch(image_instances)
-images = image_instances.data()
+    shArgs = "data/in {}".format(params.scale3sens)
 
-# create the folder structure for the folders shared with docker
-# jobFolder = baseOutputFolder + str(job.id) + "/"
-jobFolder = "/icy/data/"
-inDir = jobFolder + "in"
-outDir = jobFolder + "out"
+    command = "/icy/run.sh {}".format(shArgs)
+    call(command, shell=True)
 
-if not os.path.exists(inDir):
-    os.makedirs(inDir)
+    cj.job.update(progress=60, statusComment="Extracting polygons...")
 
-if not os.path.exists(outDir):
-    os.makedirs(outDir)
+    for image in input_images:
+        file = "{}.tif".format(image.id)
+        path = os.path.join(out_path, file)
 
-# download the images
-for image in images:
-	# url format: CYTOMINEURL/api/imageinstance/$idOfMyImageInstance/download
-	url = cytomine_host+"/api/imageinstance/" + str(image.id) + "/download"
-	filename = str(image.id) + ".tif"
-	conn.fetch_url_into_file(url, inDir+"/"+filename, True, True)
+        data = io.imread(path)
+        indexes = np.unique(data)
+        loc = locator.BinaryLocator()
+        objects = dict()
 
-# call the image analysis workflow in the docker image
-shArgs = "data/in "+scale3sens
-job = conn.update_job_status(job, status = job.RUNNING, progress = 25, status_comment = "Launching workflow...")
+        for i, index in enumerate(indexes):
+            if not index == 0:
+                mask = (data == index).astype(np.uint8) * 255
+                polygons = [polygon[0].buffer(2.0) for polygon in loc.locate(mask)]
+                polygon = cascaded_union(polygons).buffer(-2.0)
+                if not polygon.is_empty and polygon.area > 0:
+                    objects[index] = polygon
 
-command = "/icy/run.sh " + shArgs
+        print("Found {} polygons in this image {}.".format(len(objects), image.id))
 
-#command = "docker run --rm -v "+jobFolder+":/icy/data neubiaswg5/w_spotdetection-icy " + shArgs
-call(command,shell=True)	# waits for the subprocess to return
-
-# remove existing annotations if any
-for image in images:
-	annotations = conn.get_annotations(id_image=image.id)
-	for annotation in annotations:
-		conn.delete_annotation(annotation.id)
-
-files = os.listdir(outDir)
-
-job = conn.update_job_status(job, status = job.RUNNING, progress = 50, status_comment = "Extracting polygons...")
-
-for image in images:
-	file = str(image.id) + "_results.txt"
-	path = inDir + "/" + file
-	if(os.path.isfile(path)):
-		(X,Y) = readcoords(path)
-		for i in range(len(X)):
-			circle = Point(X[i],image.height-Y[i])
-			annotation.location=circle.wkt
-			new_annotation = conn.add_annotation(annotation.location, image.id)
-	else:
-		print(path + " does not exist")
+        # upload
+        for index, polygon in objects.items():
+            annotation = add_annotation(image, polygon)
+            if annotation:
+                Property(annotation, "index", str(index)).save()
 
 
-#should launch the metrics computation here
+    cj.job.update(progress=99, statusComment="Cleaning...")
+    for image in input_images:
+        os.remove(os.path.join(in_path, "{}.tif".format(image.id)))
 
-# cleanup - remove the downloaded images and the images created by the workflow
-job = conn.update_job_status(job, status = job.TERMINATED, progress = 90, status_comment =  "Cleaning up..")
-
-for image in images:
-	file = str(image.id) + ".tif"
-	#path = outDir + "/" + file
-	#os.remove(path);
-	path = inDir + "/" + file
-	os.remove(path);
-	path = inDir + "/" + str(image.id) + "_results.txt"
-	os.remove(path)
-
-job = conn.update_job_status(job, status = job.TERMINATED, progress = 100, status_comment =  "Finished Job..")
+    cj.job.update(status=Job.TERMINATED, progress=100, statusComment="Finished.")
