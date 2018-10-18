1ee46a36c7723f5cc5a204dc5b80ebedf5078b7d
Romain Mormont <romainmormont@hotmail.com>
2018-10-18 16:28:10 +0200

    working version

--- wrapper.py
+++ wrapper.py
@@ -1,9 +1,17 @@
-import sys
 import os
+import sys
+import cv2
+import numpy as np
+import tifffile
 from cytomine import CytomineJob
 from cytomine.models import *
 from subprocess import call
+
+from shapely.affinity import affine_transform
 from shapely.geometry import Point
+from annotation_exporter import csv_to_points, slices_to_mask, AnnotationSlice
+
+from neubiaswg5.metrics import computemetrics_batch
 
 
 def makedirs(path):
@@ -19,7 +27,6 @@ def readcoords(fname):
     for index, l in enumerate(F.readlines()):
         if index < 2: continue
         t = l.split('\t')
-        print()
         if len(t) > 1:
             X.append(float(t[5]))
             Y.append(float(t[6]))
@@ -29,57 +36,98 @@ def readcoords(fname):
 
 
 def main():
-    base_path = "{}".format(os.getenv("HOME")) # Mandatory for Singularity
-
     with CytomineJob.from_cli(sys.argv[1:]) as cj:
         scale3sens = cj.parameters.icy_scale3sensitivity
 
-        working_path = os.path.join(base_path, "data", str(cj.job.id))
-        in_dir = os.path.join(working_path, "in")
-        makedirs(in_dir)
-        out_dir = os.path.join(working_path, "out")
-        makedirs(out_dir)
+        cj.job.update(status=Job.RUNNING, progress=0, statusComment="Initialisation...")
 
-        cj.job.update(progress=1, statusComment="Downloading images (to {})...".format(in_dir))
-        image_instances = ImageInstanceCollection().fetch_with_filter("project", cj.project.id)
+        # 1. Create working directories on the machine:
+        # - WORKING_PATH/in: input images
+        # - WORKING_PATH/out: output images
+        # - WORKING_PATH/ground_truth: ground truth images
+        # - WORKING_PATH/tmp: temporary path
+        base_path = "{}".format(os.getenv("HOME"))
+        gt_suffix = "_lbl"
+        working_path = os.path.join(base_path, str(cj.job.id))
+        in_path = os.path.join(working_path, "in")
+        out_path = os.path.join(working_path, "out")
+        gt_path = os.path.join(working_path, "ground_truth")
+        tmp_path = os.path.join(working_path, "tmp")
 
-        for image in image_instances:
-            image.download(os.path.join(in_dir, "{id}.tif"))
+        if not os.path.exists(working_path):
+            os.makedirs(working_path)
+            os.makedirs(in_path)
+            os.makedirs(out_path)
+            os.makedirs(gt_path)
+            os.makedirs(tmp_path)
+
+        # 2. Download the images (first input, then ground truth image)
+        cj.job.update(progress=1, statusComment="Downloading images (to {})...".format(in_path))
+        image_instances = ImageInstanceCollection().fetch_with_filter("project", cj.parameters.cytomine_id_project)
+        input_images = [i for i in image_instances if gt_suffix not in i.originalFilename]
+        gt_images = [i for i in image_instances if gt_suffix in i.originalFilename]
+
+        for input_image in input_images:
+            input_image.download(os.path.join(in_path, "{id}.tif"))
+
+        for gt_image in gt_images:
+            related_name = gt_image.originalFilename.replace(gt_suffix, '')
+            related_image = [i for i in input_images if related_name == i.originalFilename]
+            if len(related_image) == 1:
+                gt_image.download(os.path.join(gt_path, "{}.tif".format(related_image[0].id)))
 
         cj.job.update(progress=25, statusComment="Launching workflow...")
-        call("java -cp /icy/lib/ -jar /icy/icy.jar -hl", shell=True)
-        call("cd /icy && java -cp /icy/lib/ -jar /icy/icy.jar -hl -x plugins.adufour.protocols.Protocols "
+        call("java -cp /icy/lib/ -jar /icy/icy.jar -hl", shell=True, cwd="/icy")
+        call("java -cp /icy/lib/ -jar /icy/icy.jar -hl -x plugins.adufour.protocols.Protocols "
              "protocol=\"/icy/protocols/protocol.protocol\" inputFolder=\"{}\" extension=tif csvFileSuffix=_results "
-             "scale3enable=true scale3sensitivity={}".format(in_dir, scale3sens), shell=True)
-
-        # # remove existing annotations if any
-        # for image in cj.monitor(image_instances, start=60, end=75, period=0.1, prefix="Delete previous annotations"):
-        #     annotations = AnnotationCollection.fetch_with_filter({"image": image.id})
-        #     for annotation in annotations:
-        #         annotation.delete()
+             "scale3enable=true scale3sensitivity={}".format(in_path, scale3sens), shell=True, cwd="/icy")
 
         cj.job.update(progress=75, status_comment="Extracting polygons...")
-        annotations = AnnotationCollection()
-        for image in cj.monitor(image_instances, start=75, end=95, period=0.1, prefix="Upload annotations"):
-            file = str(image.id) + "_results.txt"
-            path = os.path.join(in_dir, file)
-            if os.path.isfile(path):
-                (X, Y) = readcoords(path)
-                for i in range(len(X)):
-                    center = Point(X[i], image.height - Y[i])
-                    annotations.append(Annotation(location=center.wkt, id_image=image.id,
-                                                  id_project=cj.parameters.cytomine_id_project))
 
-                    if len(annotations) % 100 == 0:
-                        annotations.save()
-                        annotations = AnnotationCollection()
-            else:
+        for image in cj.monitor(input_images, start=75, end=90, period=0.1, prefix="Upload annotations and generate masks"):
+            file = str(image.id) + "_results.txt"
+            path = os.path.join(in_path, file)
+            if not os.path.isfile(path):
                 print("No output file at '{}' for image with id:{}.".format(path, image.id), file=sys.stderr)
-        # Save last annotations
-        annotations.save()
+
+                mask = np.zeros((image.height, image.width), dtype=np.uint8)
+                cv2.imwrite(os.path.join(out_path, "{}.png".format(image.id)), mask)
+                continue
+
+            # export points to AnnotationSlice
+            # points = csv_to_points(
+            #     path, has_headers=True,
+            #     parse_fn=lambda l, sep: [float(coord) for coord in l.split(sep)[5:7]]
+            # )
+            xs, ys = readcoords(path)
+            points = [AnnotationSlice(Point(x, y), label=None) for x, y in zip(xs, ys)]
+
+            annotations = AnnotationCollection()
+            for _slice in points:
+                annotations.append(Annotation(
+                    location=affine_transform(_slice.polygon, [1, 0, 0, -1, 0, image.height]).wkt,
+                    id_image=image.id,
+                    id_project=cj.parameters.cytomine_id_project
+                ))
+            annotations.save()
+
+            # create masks for metric computations
+            mask = slices_to_mask(points, (image.height, image.width))
+            tifffile.imsave(os.path.join(out_path, "{}.tif".format(image.id)), mask)
 
         # Launch the metrics computation here
-        # TODO
+        cj.job.update(progress=95, statusComment="Computing and uploading metrics...")
+        outfiles, reffiles = zip(*[
+            (os.path.join(out_path, "{}.tif".format(image.id)),
+             os.path.join(gt_path, "{}.tif".format(image.id)))
+            for image in input_images
+        ])
+
+        results = computemetrics_batch(outfiles, reffiles, "SptCnt", tmp_path)
+
+        for key, value in results.items():
+            Property(cj.job, key=key, value=str(value)).save()
+        Property(cj.job, key="IMAGE_INSTANCES", value=str([im.id for im in input_images])).save()
 
         cj.job.update(progress=100, status=Job.TERMINATED, status_comment="Finished.")
 
