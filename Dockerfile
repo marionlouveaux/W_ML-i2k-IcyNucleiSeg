8f9cb90a63fb4168c983e89214109e648cbf5fc0
volker-baecker <volker.baecker@mri.cnrs.fr>
2019-03-01 16:49:14 +0100

    fixed the icy spot detection and removed the base-images

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,59 @@
-FROM neubiaswg5/icy-base:latest
+FROM python:3.6
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Install Cytomine python client
+RUN git clone https://github.com/cytomine-uliege/Cytomine-python-client.git
+RUN cd /Cytomine-python-client && git checkout tags/v2.2.0 && pip install .
+RUN rm -r /Cytomine-python-client
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Install Neubias-W5-Utilities (annotation exporter, compute metrics, helpers,...)
+RUN git clone https://github.com/Neubias-WG5/neubiaswg5-utilities.git
+RUN cd /neubiaswg5-utilities/ && git checkout tags/v0.5.2a && pip install .
+
+# install utilities binaries
+RUN chmod +x /neubiaswg5-utilities/bin/*
+RUN cp /neubiaswg5-utilities/bin/* /usr/bin/
+
+# cleaning
+RUN rm -r /neubiaswg5-utilities
+
+# Install Java
+RUN apt-get update && apt-get install openjdk-8-jdk -y && apt-get clean
+
+# Install Neubias-W5-Utilities (annotation exporter, compute metrics, helpers,...)
+RUN git clone https://github.com/Neubias-WG5/neubiaswg5-utilities.git
+RUN chmod +x /neubiaswg5-utilities/bin/*
+RUN cp /neubiaswg5-utilities/bin/* /usr/bin/
+RUN cd /neubiaswg5-utilities/ && pip install .
+RUN rm -r /neubiaswg5-utilities
+
+# keep re-install of numpy as long as this is open: https://github.com/scikit-image/scikit-image/issues/3586
+# latest scikit-image is incompatible with latest numpy (1.16) on pip. This is being fixed by skimage team.
+
+RUN pip install numpy==1.13.0
+RUN pip install Cython==0.29.6
+RUN pip install imagecodecs-lite==2019.2.22 
+
+# Java is installed in neubiaswg5/neubias-base.
+
+# Install Icy.
+RUN apt-get update && apt-get install -y unzip wget && \
+    mkdir -p /icy && \
+    cd /icy && \
+    wget -O icy.zip http://www.icy.bioimageanalysis.org/downloadIcy/icy_1.9.9.1.zip && \
+    unzip icy.zip && \
+    rm -rf icy.zip
+      
+#ADD icy_patch.jar /icy/icy.jar
+
+# Add icy to the PATH
+ENV PATH $PATH:/icy
+
+RUN mkdir -p /icy/data/in && \
+mkdir -p /icy/protocols
+
+# add workflow specific files
 
 ADD protocol.protocol /icy/protocols/protocol.protocol
 ADD wrapper.py /app/wrapper.py
--- wrapper.py
+++ wrapper.py
@@ -1,12 +1,11 @@
 import os
-import shutil
 import sys
 from cytomine.models import *
 from subprocess import call
+from pathlib import Path
 
 from neubiaswg5 import CLASS_SPTCNT
 from neubiaswg5.helpers import prepare_data, NeubiasJob, upload_data, upload_metrics
-
 
 def makedirs(path):
     if not os.path.exists(path):
@@ -28,6 +27,7 @@ def main():
         # 1. Create working directories on the machine:
         # 2. Download (or read) data
         problem_cls = CLASS_SPTCNT
+        is_2d = True
         in_images, gt_images, in_path, gt_path, out_path, tmp_path = prepare_data(problem_cls, nj, is_2d=True, **nj.flags)
 
         # 3. Execute workflow
@@ -35,36 +35,20 @@ def main():
         nj.job.update(progress=25, statusComment="Launching workflow...")
         call("java -cp /icy/lib/ -jar /icy/icy.jar -hl", shell=True, cwd="/icy")
         call("java -cp /icy/lib/ -jar /icy/icy.jar -hl -x plugins.adufour.protocols.Protocols "
-             "protocol=\"/icy/protocols/protocol.protocol\" inputFolder=\"{}\" extension=tif csvFileSuffix=_results "
-             "scale3enable=true scale3sensitivity={}".format(in_path, scale3sens), shell=True, cwd="/icy")
+             "protocol=\"/icy/protocols/protocol.protocol\" inputFolder=\"{}\" outputFolder=\"{}\" extension=tif "
+             "scale2enable=true scale2sensitivity={}".format(in_path, out_path, scale3sens), shell=True, cwd="/icy")
 
-        # Generated csv files must be cleaned: remove invalid header '== ROI Statistics ==' in result file (i.e.
-        # remove one line). Also, move CSV file to output folder.
-        # If --nodownload is set, image is a filepath. Otherwise, it is an ImageInstance.
-        csv_suffix = "_results.txt"
-        for image in in_images:
-            if isinstance(image, str):
-                filename = os.path.join(in_path, os.path.basename(image).rsplit(".", 1)[0])
-            else:
-                filename = os.path.join(in_path, str(image.id))
-            filename += csv_suffix
-            clean_icy_result_file(filename, n=1)
-            shutil.move(filename, os.path.join(out_path, os.path.basename(filename)))
-
+        # 3.5 Remove the xml-output files
+        for p in Path(out_path).glob("*.xml"):
+            p.unlink()
+        
         # 4. Upload the annotation and labels to Cytomine
-        upload_data(
-            problem_cls, nj, in_images, out_path,
-            is_csv=True, result_file_suffix=csv_suffix,
-            generate_mask=True, in_path=in_path,
-            has_headers=True,
-            parse_fn=lambda l, sep: [float(coord) for coord in l.split(sep)[5:7]],
-            **nj.flags, monitor_params={
-                "start": 75, "end": 90, "period": 0.1
-            }
-        )
+        upload_data(problem_cls, nj, in_images, out_path, **nj.flags, is_2d=is_2d, monitor_params={
+           "start": 60, "end": 90, "period": 0.1
+        })
 
-        # Launch the metrics computation here
-        nj.job.update(progress=95, statusComment="Computing and uploading metrics...")
+        # 5. Compute and upload the metrics
+        nj.job.update(progress=90, statusComment="Computing and uploading metrics...")
         upload_metrics(problem_cls, nj, in_images, gt_path, out_path, tmp_path, **nj.flags)
 
         nj.job.update(progress=100, status=Job.TERMINATED, status_comment="Finished.")
